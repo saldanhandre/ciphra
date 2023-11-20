811d7a0a474c0f0764f6d98394a60e0d94475d80
saldanhandre <r.saldanha@lancaster.ac.uk>
2023-11-20 14:47:07 +0000

    Cistercian to Arabic conversion feature

--- app/src/main/java/com/example/uidesign_cistercian/MainActivity.java
+++ app/src/main/java/com/example/uidesign_cistercian/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.uidesign_cistercian;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
 import java.util.Map;
 import java.util.HashMap;
 
@@ -24,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
 
         // Set up the click listeners for each segment
         initializeSegmentClickListeners();
+
+        Button conversionButton = findViewById(R.id.cistArabConversionButton);
+        conversionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int arabicNumber = convertCistercianToArabic();
+                displayArabicNumber(arabicNumber);
+            }
+        });
     }
 
     private void initializeSegmentRelations() {
@@ -183,5 +197,104 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return false;
+    }
+
+    private int convertCistercianToArabic() {
+        // Initialize the number for each quadrant
+        int units = 0, tens = 0, hundreds = 0, thousands = 0;
+
+        // Check the segments for the units
+        if (findViewById(R.id.segment1).isSelected()) units += 1;
+        if (findViewById(R.id.segment2).isSelected()) units += 2;
+        if (findViewById(R.id.segment3_1).isSelected() || findViewById(R.id.segment3_2).isSelected()) units += 3;
+        if (findViewById(R.id.segment4_1).isSelected() || findViewById(R.id.segment4_2).isSelected()) units += 4;
+        // Check if segment5 is selected along with segments 1 or 2 for numbers 7 and 8
+        if (findViewById(R.id.segment5).isSelected()) {
+            if (units == 1) {
+                units = 7; // segment1 and segment5
+            } else if (units == 2) {
+                units = 8; // segment2 and segment5
+            } else if (units == 3) {
+                units = 9; // segment1, segment2 and segment5
+            } else if (units == 0) {
+                units = 6; // only segment5
+            } // If units is already 3 or 4, we do not change it because segment5 cannot combine with segment3 or segment4
+        }
+
+        // Check the segments for the tens
+        if (findViewById(R.id.segment6).isSelected()) tens += 10;
+        if (findViewById(R.id.segment7).isSelected()) tens += 20;
+        if (findViewById(R.id.segment8_1).isSelected() || findViewById(R.id.segment8_2).isSelected()) tens += 30;
+        if (findViewById(R.id.segment9_1).isSelected() || findViewById(R.id.segment9_2).isSelected()) tens += 40;
+        if (findViewById(R.id.segment10).isSelected()) {
+            if (tens == 10) {
+                tens = 70;
+            } else if (tens == 20) {
+                tens = 80;
+            } else if (tens == 30) {
+                tens = 90;
+            } else {
+                tens = 60;
+            }
+        }
+
+        // Check the segments for the hundreds
+        if (findViewById(R.id.segment11).isSelected()) hundreds += 100;
+        if (findViewById(R.id.segment12).isSelected()) hundreds += 200;
+        if (findViewById(R.id.segment13_1).isSelected() || findViewById(R.id.segment13_2).isSelected()) hundreds += 300;
+        if (findViewById(R.id.segment14_1).isSelected() || findViewById(R.id.segment14_2).isSelected()) hundreds += 400;
+        if (findViewById(R.id.segment15).isSelected()) {
+            if (hundreds == 100) {
+                hundreds = 700;
+            } else if (hundreds == 200) {
+                hundreds = 800;
+            } else if (hundreds == 300) {
+                hundreds = 900;
+            } else {
+                hundreds = 600;
+            }
+        }
+
+        // Check the segments for the thousands
+        if (findViewById(R.id.segment16).isSelected()) thousands += 1000;
+        if (findViewById(R.id.segment17).isSelected()) thousands += 2000;
+        if (findViewById(R.id.segment18_1).isSelected() || findViewById(R.id.segment18_2).isSelected()) thousands += 3000;
+        if (findViewById(R.id.segment19_1).isSelected() || findViewById(R.id.segment19_2).isSelected()) thousands += 4000;
+        if (findViewById(R.id.segment20).isSelected()) {
+            if (thousands == 1000) {
+                thousands = 7000;
+            } else if (thousands == 2000) {
+                thousands = 8000;
+            } else if (thousands == 3000) {
+                thousands = 9000;
+            } else {
+                thousands = 6000;
+            }
+        }
+
+        int arabicResult = thousands + hundreds + tens + units;
+        return arabicResult;
+    }
+
+    private void displayArabicNumber(int number) {
+        // Create an AlertDialog builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // Inflate the custom layout
+        View dialogView = getLayoutInflater().inflate(R.layout.cistercian_conversion_layout, null);
+        // Find the TextView and set the number
+        TextView resultTextView = dialogView.findViewById(R.id.resultTextView);
+        resultTextView.setText(String.valueOf(number));
+        // Set the custom layout on the dialog builder
+        builder.setView(dialogView);
+        // Add an OK button to dismiss the dialog
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+                dialog.dismiss();
+            }
+        });
+        // Create and show the AlertDialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
