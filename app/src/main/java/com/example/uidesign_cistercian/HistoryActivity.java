db69cb3a206e7b99b24a4f4aa5e03e2cca1684d2
saldanhandre <r.saldanha@lancaster.ac.uk>
2023-12-11 11:57:40 +0000

    Bug fixes. 2 way conversion now in the same EditText

--- app/src/main/java/com/example/uidesign_cistercian/HistoryActivity.java
+++ app/src/main/java/com/example/uidesign_cistercian/HistoryActivity.java
@@ -1,11 +1,11 @@
 package com.example.uidesign_cistercian;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
+import android.view.MenuItem;
+import java.util.Collections;
 import java.util.List;
 
 public class HistoryActivity extends AppCompatActivity {
@@ -15,12 +15,31 @@ public class HistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
+        // Enable back button
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
         ListView historyListView = findViewById(R.id.historyListView);
         List<Integer> history = ConversionHistoryManager.getInstance().getConversionHistory();
+
+        // Reverse the list
+        Collections.reverse(history);
 
         // Use an ArrayAdapter to show the history in the ListView
         ArrayAdapter<Integer> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, history);
         historyListView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle the Up button click here
+        if (item.getItemId() == android.R.id.home) {
+            // Finish this activity and return to the parent activity (MainActivity)
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
