c48230c8ee712df0de2956101f0b59b0239bd0da
saldanhandre <r.saldanha@lancaster.ac.uk>
2024-01-17 23:28:30 +0000

    Image being processed in B&W

--- app/src/main/java/com/example/uidesign_cistercian/ImageDisplayActivity.java
+++ app/src/main/java/com/example/uidesign_cistercian/ImageDisplayActivity.java
@@ -4,13 +4,37 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
 import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class ImageDisplayActivity extends AppCompatActivity {
+
+    private Mat matImage;
+
+    // Load openCV library
+    static {
+        if (!OpenCVLoader.initDebug()) {
+            Log.e("OpenCV", "Unable to load OpenCV");
+            // Handle the failure here
+        } else {
+            Log.d("OpenCV", "OpenCV loaded successfully");
+        }
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,13 +54,49 @@ public class ImageDisplayActivity extends AppCompatActivity {
                 // Image is passed as a URI
                 Uri imageUri = Uri.parse(extras.getString("imageUri"));
                 imageView.setImageURI(imageUri);
+                convertToGrayscale(imageUri);
             } else if (extras.containsKey("imageBitmap")) {
                 // Image is passed as a byte array
                 byte[] byteArray = extras.getByteArray("imageBitmap");
                 Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
                 imageView.setImageBitmap(bmp);
+                convertToGrayscale(bmp);
             }
         }
+    }
+
+    private void convertToGrayscale(Uri imageUri) {
+        try {
+            // Convert Uri to Bitmap
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+            convertToGrayscale(bitmap);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            showToast("File not found. Please try again.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            showToast("Error processing the image. Please try again.");
+        }
+    }
+
+    private void showToast(String message) {
+        runOnUiThread(() -> Toast.makeText(ImageDisplayActivity.this, message, Toast.LENGTH_LONG).show());
+    }
+
+    private void convertToGrayscale(Bitmap bitmap) {
+        // Initialize matImage with the size of bitmap
+        matImage = new Mat(bitmap.getWidth(), bitmap.getHeight(), CvType.CV_8UC1);
+        // Convert Bitmap to Mat
+        Utils.bitmapToMat(bitmap, matImage);
+        // Convert to Grayscale
+        Imgproc.cvtColor(matImage, matImage, Imgproc.COLOR_RGB2GRAY);
+        // Convert back to Bitmap to display in ImageView or further processing
+        Utils.matToBitmap(matImage, bitmap);
+        // Update the ImageView
+        runOnUiThread(() -> {
+            ImageView imageView = findViewById(R.id.image_display_view);
+            imageView.setImageBitmap(bitmap);
+        });
     }
 }
 
